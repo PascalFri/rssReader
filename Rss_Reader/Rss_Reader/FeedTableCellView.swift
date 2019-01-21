e4f2d16f27c68cfee2589a3f8a89b23fcce6d383
PascalFri <45208101+PascalFri@users.noreply.github.com>
2019-01-21 19:50:18 +0100

    restart of the GUI

---
+++ Rss_Reader/Rss_Reader/FeedTableCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedTableCellView.swift
+//  Rss_Reader
+//
+//  Created by Pascal Fritz on 21.01.19.
+//
+
+import Cocoa
+
+class FeedTableCellView: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
